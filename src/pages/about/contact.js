a26d7d262682c70f0c4682e7f733c4fd720ea191
Shreyas Jaganmohan <shreyas@checkr.com>
2018-04-16 21:43:39 -0700

    extremely important

--- src/pages/about/contact.js
+++ src/pages/about/contact.js
@@ -11,7 +11,7 @@ const AboutContactPage= () => (
     <p> <b>LinkedIn</b>: <a href="https://www.linkedin.com/in/shreyas-jaganmohan">linkedin.com/in/shreyas-jaganmohan</a></p>
     <p>
         If you want to talk to me about anything, feel free to send me an email. Check out my Github if you want to see how
-        I make stuff, listen to some of my music on SoundCloud, read my <i> extremely important</i> thoughts about the world on Twitter.
+        I make stuff, listen to some of my music on SoundCloud, and absolutely read my <i> extremely important</i> thoughts about the world on Twitter.
     </p>
   </div>
 )
